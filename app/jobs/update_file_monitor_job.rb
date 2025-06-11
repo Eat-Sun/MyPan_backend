892bbe84d6ab3a4ca36b1bcf6253e0bf2ccac8e7
TyGG <879705054@qq.com>
2025-06-11 10:38:33 +0800

    修改了

--- app/jobs/update_file_monitor_job.rb
+++ app/jobs/update_file_monitor_job.rb
@@ -1,18 +1,30 @@
 class UpdateFileMonitorJob < ApplicationJob
+  def perform(*_args)
+    redis = Redis.new(url: ENV['CACHE_URL'] || 'redis://localhost:6379/0',
+                      password: Rails.application.credentials.dig(:REDIS_PASSWORD))
 
-  def perform(*args)
-    redis = Redis.new(url: ENV['CACHE_URL'] || 'redis://localhost:6379/0', password: Rails.application.credentials.dig(:REDIS_PASSWORD))
-    b2_keys = redis.smembers(Attachment::Initial::Monitor)
-    if b2_keys.any?
-      key_values = redis.mapped_mget(*b2_keys)
-      opt = key_values.map do |key, value|
-        Rails.cache.decrement(key, value)
-        { b2_key: key, owner_count: value }
-      end
-
-      reulst = FileMonitor.upsert_all(opt, unique_by: :b2_key)
-    else
-      return
+    opt = []
+    while (key = redis.spop(Attachment::Initial::Monitor))
+      value = redis.getdel key
+      opt << { b2_key: key, owner_count: value }
     end
+
+    result = FileMonitor.upsert_all(opt, unique_by: :b2_key, returning: Arel.sql('b2_key, id as file_monitor_id'))
+    update_attachement result unless result.empty?
+  end
+
+  def update_attachement(result)
+    values = result.map do |h|
+      "(#{h['file_monitor_id']}, '#{ActiveRecord::Base.connection.quote_string(h['b2_key'])}')"
+    end.join(',')
+
+    sql = <<~SQL
+      UPDATE attachments AS a
+      SET file_monitor_id = v.file_monitor_id
+      FROM (VALUES #{values}) AS v(file_monitor_id, b2_key)
+      WHERE a.b2_key = v.b2_key
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
   end
 end
--- app/models/file_monitor.rb
+++ app/models/file_monitor.rb
@@ -1,22 +1,22 @@
 class FileMonitor < ApplicationRecord
   has_many :attachments
-	validates :owner_count, presence: { message: "所有者数量不能为空"}
-	validate :attachment_must_be_present
+  validates :owner_count, presence: { message: '所有者数量不能为空' }
+  validate :attachment_must_be_present
 
   def self.need_to_destroy
     need_delete_file_monitors = FileMonitor.where(owner_count: 0)
     b2_keys = need_delete_file_monitors.map { |file_monitor| { key: file_monitor.b2_key } }
 
-    need_delete_file_monitors.destroy_all
+    need_delete_file_monitors.delete_all
 
-    return b2_keys
+    b2_keys
   end
 
   private
-    def attachment_must_be_present
-      if self.attachments.empty?
-        errors.add(:base, "必须至少选择一个 attachment")
-      end
-    end
 
+  def attachment_must_be_present
+    return unless attachments.empty?
+
+    errors.add(:base, '必须至少选择一个 attachment')
+  end
 end
